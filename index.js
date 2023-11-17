aca36f3b374e535fa12276f23293a4fcce8802ea
probeshnath <probeshn@gmail.com>
2023-11-18 02:51:18 +0600

    cart page added and delete cart item

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3rjzxua.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,13 +54,18 @@ async function run() {
       res.send(result);
      })
 
-
     app.post("/carts", async(req,res)=>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     })
 
+    app.delete("/carts/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
