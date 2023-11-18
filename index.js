a3151068ceb5a0782648486dced2dde40c5c9f54
probeshnath <probeshn@gmail.com>
2023-11-19 00:09:53 +0600

    delete and make admin user

--- index.js
+++ index.js
@@ -88,6 +88,25 @@ async function run() {
         res.send(result)
       })
 
+      app.delete('/users/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await userCollection.deleteOne(query)
+        res.send(result)
+      })
+
+      app.patch("/users/admin/:id", async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updatedDoc = {
+          $set:{
+            role:"admin"
+          }
+        }
+        const result = await userCollection.updateOne(filter,updatedDoc)
+        res.send(result)
+      })
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
